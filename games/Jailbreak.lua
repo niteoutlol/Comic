dcdb5a5fbe8c0dc9d0cc963331deae266b6fcfd5
JohanDev <johanclipss@gmail.com>
2022-03-29 20:58:34 +0200

    Update Jailbreak.lua

--- games/Jailbreak.lua
+++ games/Jailbreak.lua
@@ -1,5 +1,7 @@
 print("Comic - Jailbreak")
 
+local hrpc = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame
+
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GreenDeno/Venyx-UI-Library/main/source.lua"))()
 local window = library.new("Comic", 5013109572)
 
@@ -15,13 +17,28 @@ local themes = {
 local character = window:addPage("Character", 5012544693)
 local movement = character:addSection("Movement")
 
-shared.cwalkspeed = 16
+shared.cwalkspeed = 16 
 
-movement:addToggle("WalkSpeed", false, function(value)
-    while value do
-        game.Players.LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed = shared.cwalkspeed
+-- Dette scriptet cræsjer gamet når man bruker kato api
+
+movement:addToggle("WalkSpeed", nil, function(value)
+    if value then
+        while value do
+            game.Players.LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed = shared.cwalkspeed
+            wait()
+        end
+        game.Players.LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed = 16
+    else
+        game.Players.LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed = 16
     end
 end)
 movement:addSlider("WalkSpeed", 16, 16, 256, function(value)
     shared.cwalkspeed = value
+end)
+
+local teleportation = window:addPage("Teleportation", 5012544693)
+local teleport = teleportation:addSection("Teleport")
+
+teleport:addButton("tp", function()
+    repeat wait() hrpc = CFrame.new(hrcp =+ 1, 300, hrcp =+ 1) until hrcp == CFrame.new(20, 300, 70)
 end)
